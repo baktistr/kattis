b28b4c74b655b8b07ee57021e453b22560572cbb
bakti satria adhityatama <bakti.satria.a@gmail.com>
2023-11-01 20:31:25 +0700

    using set solutions

--- set/cd.py
+++ set/cd.py
@@ -2,34 +2,30 @@
 from sys import stdin
 
 while True:
-    n, m = map(int, input().split())
-    # n, m = map(int, stdin.readline().split())
+    n, m = map(int, stdin.readline().split())
     if n == 0 and m == 0:
         break
 
-    # jack = [int(input()) for _ in range(n)]
-    # jill = [int(input()) for _ in range(m)]
-    jack = [int(stdin.readline()) for _ in range(n)]
-    jill = [int(stdin.readline()) for _ in range(m)]
+    # jack = [int(stdin.readline()) for _ in range(n)]
+    # jill = [int(stdin.readline()) for _ in range(m)]
 
-    # jack = []
-    # for _ in range(n):
-    #     jack.append(int(stdin.readline()))
+    # i, j, count = 0, 0, 0
 
-    # jill = []
-    # for _ in range(m):
-    #     jill.append(int(stdin.readline()))
+    # while i < n and j < m:
+    #     if jack[i] == jill[j]:
+    #         count += 1
+    #         i += 1
+    #         j += 1
+    #     elif jack[i] < jill[j]:
+    #         i += 1
+    #     else:
+    #         j += 1
 
-    i, j, count = 0, 0, 0
+    # print(count)
 
-    while i < n and j < m:
-        if jack[i] == jill[j]:
-            count += 1
-            i += 1
-            j += 1
-        elif jack[i] < jill[j]:
-            i += 1
-        else:
-            j += 1
+    jack = set(int(stdin.readline()) for _ in range(n))
+    jill = set(int(stdin.readline()) for _ in range(m))
 
-    print(count)
+    common_cds = jack.intersection(jill)
+
+    print(len(common_cds))
